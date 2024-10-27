f17f3348fe2ca9666ae0caec579cf5411226482a
Giddel Wilson <134491076+Giddel-Wilson@users.noreply.github.com>
2024-10-27 12:41:57 +0100

    Add files via upload

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Homeshell from "./homeshell.svelte";
+    import AddressShell from "./addressShell.svelte";
+    import UpdateShell from "./updateShell.svelte";
+    import NewsletterShell from "./newsletterShell.svelte";
+    import FooterShell from "./footerShell.svelte";
+</script>
+
+<main class="w-full h-full">
+    <Homeshell />
+    <AddressShell />
+    <UpdateShell />
+    <NewsletterShell />
+    <FooterShell />
+</main>
